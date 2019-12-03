428a2a5a3d90d037f70466db168e5bd6bdc65936
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-02 21:35:53 -0700

    Add shelter validation

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,6 @@
 class Shelter < ApplicationRecord
   has_many :pets,  dependent: :destroy
+  has_many :reviews,  dependent: :destroy
+
+  validates_presence_of :name, :address, :city, :state, :zip
 end

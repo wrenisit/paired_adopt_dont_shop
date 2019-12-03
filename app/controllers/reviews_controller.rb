54d85bb5c206d45bcd9d74abe118dcf8b8fa2c8d
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-03 12:09:51 -0700

    Add new and create methods to reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,19 @@
 class ReviewsController < ApplicationController
+  def new
+    @shelter = Shelter.find(params[:shelter_id])
+  end
 
+  def create
+    shelter = Shelter.find(params[:shelter_id])
+    review = shelter.reviews.create(review_params)
+    review.save
+      redirect_to('/shelters/#{shelter.id}')
+  end
+
+
+  private
+
+    def review_params
+      params.permit(:title, :rating, :content)
+    end
 end

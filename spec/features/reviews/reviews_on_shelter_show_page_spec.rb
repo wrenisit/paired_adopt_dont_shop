4ee1e6a314d383e3ce380121e41991abbeff6e1f
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-03 12:09:19 -0700

    Add review test to include link for new review creation

--- spec/features/reviews/reviews_on_shelter_show_page_spec.rb
+++ spec/features/reviews/reviews_on_shelter_show_page_spec.rb
@@ -1,33 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "User can see shelter reviews", type: :feature do
-it "can see all reviews" do
-  shelter_1 = Shelter.create( name:    "Denver Dog Shelter",
-                              address: "123 Main St",
-                              city:    "Denver",
-                              state:   "CO",
-                              zip:     "80206")
-  shelter_review_1 = shelter_1.reviews.create(
-                      title:  "WOW! AWESOME SHELTER!",
-                      rating:  5,
-                      content: "I adopted my friendly pup from here! 10/10 would go back.")
-  shelter_review_2 = shelter_1.reviews.create(
-                      title:  "Cleanest shelter I've been to!",
-                      rating:  5,
-                      content: "I adopted my friendly pups from here!")
+  before(:each) do
+    @shelter_1 = Shelter.create( name:    "Denver Dog Shelter",
+                                address: "123 Main St",
+                                city:    "Denver",
+                                state:   "CO",
+                                zip:     "80206")
+    @shelter_review_1 = @shelter_1.reviews.create(
+                        title:  "WOW! AWESOME SHELTER!",
+                        rating:  5,
+                        content: "I adopted my friendly pup from here! 10/10 would go back.")
+    @shelter_review_2 = @shelter_1.reviews.create(
+                        title:  "Cleanest shelter I've been to!",
+                        rating:  5,
+                        content: "I adopted my friendly pups from here!")
 
-  visit "/shelters/#{shelter_1.id}"
+    visit "/shelters/#{@shelter_1.id}"
+  end
 
-    within "#shelter-#{shelter_review_1.id}" do
-      expect(page).to have_content("WOW! AWESOME SHELTER!")
-      expect(page).to have_content(5)
-      expect(page).to have_content("I adopted my friendly pup from here! 10/10 would go back")
-    end
+  it "can see all reviews" do
+   within "#shelter-#{@shelter_review_1.id}" do
+    expect(page).to have_content("WOW! AWESOME SHELTER!")
+    expect(page).to have_content(5)
+    expect(page).to have_content("I adopted my friendly pup from here! 10/10 would go back")
+  end
 
-    within "#shelter-#{shelter_review_2.id}" do
+    within "#shelter-#{@shelter_review_2.id}" do
       expect(page).to have_content("Cleanest shelter I've been to!")
       expect(page).to have_content(5)
       expect(page).to have_content("I adopted my friendly pups from here!")
     end
+  end
+
+  it "can click link to create a new review" do
+    expect(page).to have_link("Create new review")
+    click_on("Create new review")
+    expect(page).to have_current_path("/shelters/#{@shelter_1.id}/new_review")
   end
 end

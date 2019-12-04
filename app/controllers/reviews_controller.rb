fc5edeee7de5b1492ff83d74b6e3c379ed048861
Wren Steitle <lauren.steitle@gmail.com>
2019-12-04 08:57:25 -0700

    Complete with edit functionality for reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,17 @@
 class ReviewsController < ApplicationController
-  def new
-    @shelter = Shelter.find(params[:shelter_id])
-  end
-
-  def create
-    shelter = Shelter.find(params[:shelter_id])
-    review = shelter.reviews.create(review_params)
-    review.save
-      redirect_to('/shelters/#{shelter.id}')
-  end
-
   def destroy
     Review.destroy(params[:review_id])
+    redirect_to "/shelters/#{params[:id]}"
+  end
+
+  def edit
+    @id = params[:id]
+    @review = Review.find(params[:review_id])
+  end
+
+  def update
+    review = Review.find(params[:review_id])
+    review.update(review_params)
     redirect_to "/shelters/#{params[:id]}"
   end
 

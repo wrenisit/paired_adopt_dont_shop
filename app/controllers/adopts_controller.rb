00f29c2feacff6d05543c857a8808a1ee4a7669b
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-08 15:16:44 -0700

    Add adopts controller

---
+++ app/controllers/adopts_controller.rb
@@ -0,0 +1,18 @@
+class AdoptsController < ApplicationController
+
+  def new
+    @favorites = Pet.find(sessions[:favorites].split(','))
+  end
+
+  def create
+    application = Adopt.new(adopt_params)
+    @favorites = session[:favorites]
+  end
+
+
+  private
+
+    def adopt_params
+      params.permit(:name, :address, :city, :state, :phone, :description)
+    end
+end

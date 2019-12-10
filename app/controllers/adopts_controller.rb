251dfd98d5f3eefaa1da59bb3243664297d4cb22
Wren Steitle <lauren.steitle@gmail.com>
2019-12-09 21:29:57 -0700

    Complete through user story 16 without flash message

--- app/controllers/adopts_controller.rb
+++ app/controllers/adopts_controller.rb
@@ -5,8 +5,11 @@ class AdoptsController < ApplicationController
   end
 
   def create
-    application = Adopt.new(adopt_params)
-    @favorites = session[:favorites]
+    @application_params = adopt_params
+    @adopt_pets = pets_params
+    @adopt_pets.each do |id|
+      @favs.pets.delete_if{|key, value| key == id }
+    end
     redirect_to '/favorites'
   end
 
@@ -15,5 +18,15 @@ class AdoptsController < ApplicationController
 
     def adopt_params
       params.permit(:name, :address, :city, :state, :phone, :description)
+    end
+
+    def pets_params
+      pets_list = []
+      params.each do |key, value|
+        if key == value
+          pets_list << key
+        end
+      end
+      pets_list
     end
 end
--- app/models/adopt.rb
+++ app/models/adopt.rb
@@ -1,6 +1,14 @@
 class Adopt < ApplicationRecord
-
   has_many :pet_adopts
   has_many :pets, through: :pet_adopts
   validates_presence_of :name, :address, :city, :state, :zip, :phone, :description
+
+  def apply_for_pets
+    #Need: pets_ids, application_data
+    #creates the application
+    #for each pet checked, joins table it
+    #remove applied pets from the favs
+  end
+
+  
 end

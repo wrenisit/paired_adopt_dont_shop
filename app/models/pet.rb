d300fe44b621967917251ae5b11f56cbacfc4a77
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-11 11:37:31 -0700

    Add toggle adoption status method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,9 @@ class Pet < ApplicationRecord
   has_many :pet_adopts
   has_many :adopts, through: :pet_adopts
   validates_presence_of :image, :name, :approximate_age, :sex, :shelter, :description
+
+  def toggle_adoption_status
+    toggle(:adoptable)
+    save
+  end
 end
